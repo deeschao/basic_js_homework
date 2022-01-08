c8f9f507ac70b211b644a11d32a4aa9f7e910df8
deeschao <deeschao0123@gmail.com>
2022-01-07 16:45:14 -0800

    completed basic js

---
+++ basic.js
@@ -0,0 +1,26 @@
+var x = 7;
+var y = 8;
+console.log(x+y);
+
+var a = 10;
+var b = 20;
+console.log(a+b);
+
+var cat = 100;
+var dog = 200;
+console.log(dog/cat);
+
+var jan = 23;
+var feb = 14;
+console.log (jan*feb);
+
+var h = "hello";
+var w = "world";
+var both = h+w;
+console.log(both);
+
+
+var food = "My favorite food is ";
+var sandwich = "In and out burger";
+var favorite = food + sandwich;
+console.log(favorite);
